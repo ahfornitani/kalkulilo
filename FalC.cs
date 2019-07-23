0f004700dd49a35538f2c2ac0e444677ca6eb3f9
Augusto Fornitani <augusto.fornitani@outlook.com>
2019-07-23 10:13:52 -0300

    Sync

---
+++ FalC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalcConsoleAug;
+
+namespace CalcConsoleAug
+{
+    public class FalC
+    {
+        public FalC()
+        {
+            Console.WriteLine("omg, f al c");
+        }
+    }
+}
---
+++ kalkulilo.py
@@ -0,0 +1,10 @@
+while True:
+    entajpu = input()
+    resultListo = []
+    originala = entajpu.split("//")
+    enigita = originala[0]
+
+    listoTeksta = {"konstantopi": "3.1415926536", "konstpi": "3.1415926536"}
+
+    for ŝlosilo in listoTeksta.keys:
+        print(ŝlosilo)
